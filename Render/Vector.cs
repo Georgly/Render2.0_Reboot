012c61125d6b5a18f9718bd2b040212d1c1a3202
Georgly <korsakov.georgy.s@gmail.com>
2016-12-25 00:49:30 +0300

    Update Vector.cs

--- Render/Vector.cs
+++ Render/Vector.cs
@@ -17,8 +17,6 @@ namespace Render
             Z = z;
             X = x;
             Y = y;
-            //FinalX = Convert.ToInt32(Constants.center + (x - z * Math.Cos(Constants.angl / 180 * Math.PI) / 2) * Constants.zoom);
-            //FinalY = Convert.ToInt32(Constants.center - (y + z * Math.Sin(Constants.angl / 180 * Math.PI) / 2) * Constants.zoom);
         }
 
         double Length()
@@ -28,45 +26,29 @@ namespace Render
 
         public static Vector Subtract(Vector v1, Vector v2)
         {
-            Vector v = new Vector();
-            v.X = v1.X - v2.X;
-            v.Y = v1.Y - v2.Y;
-            v.Z = v1.Z - v2.Z;
-            return v;
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
         }
 
         public static Vector Cross(Vector v1, Vector v2)
         {
-            Vector v = new Vector();
-
-            v.X = v1.Y * v2.Z - v1.Z * v2.Y;
-            v.Y = v1.Z * v2.X - v1.X * v2.Z;
-            v.Z = v1.X * v2.Y - v1.Y * v2.X;
-
-            return v;
+            return new Vector((float)(v1.Y * v2.Z - v1.Z * v2.Y), (float)(v1.Z * v2.X - v1.X * v2.Z), (float)(v1.X * v2.Y - v1.Y * v2.X));
         }
 
         public static Vector Normalize(Vector v)
         {
-            Vector vec = new Vector((float)(v.X / v.Length()), (float)(v.Y / v.Length()), (float)(v.Z / v.Length()));
-            return vec;
+            return new Vector((float)(v.X / v.Length()), (float)(v.Y / v.Length()), (float)(v.Z / v.Length()));
         }
 
         public static float Dot(Vector v1, Vector v2)
         {
-            float dot = v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;
-            return dot;
+            return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;
         }
 
         public static Vector TransformCoordinate(Vector coord, Matrix transMat)
         {
-            Vector v = new Vector();
-
-            v.X = transMat.M11 * coord.X + transMat.M12 * coord.Y + transMat.M13 * coord.Z + transMat.M14 * 1;
-            v.Y = transMat.M21 * coord.X + transMat.M22 * coord.Y + transMat.M23 * coord.Z + transMat.M24 * 1;
-            v.Z = transMat.M31 * coord.X + transMat.M32 * coord.Y + transMat.M33 * coord.Z + transMat.M34 * 1;
-
-            return v;
+            return new Vector(transMat.M11 * coord.X + transMat.M12 * coord.Y + transMat.M13 * coord.Z + transMat.M14 * 1,
+                              transMat.M21 * coord.X + transMat.M22 * coord.Y + transMat.M23 * coord.Z + transMat.M24 * 1,
+                              transMat.M31 * coord.X + transMat.M32 * coord.Y + transMat.M33 * coord.Z + transMat.M34 * 1);
         }
 
         //internal static Vector UnitY()
